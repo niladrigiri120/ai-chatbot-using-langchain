e487112e9e18dc033a06a9186cb1ae344c9c89b9
Niladri Giri <niladrigiri120@gmail.com>
2026-02-27 14:06:59 +0530

    Include API key for ChatGroq model
    Added API key retrieval for ChatGroq model initialization.

--- langchain_chatbot.py
+++ langchain_chatbot.py
@@ -3,10 +3,9 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 model = ChatGroq(
     model= "openai/gpt-oss-20b",
+    api_key= os.getenv(Groq_api_key)
     streaming= True
     )
 
@@ -21,6 +20,7 @@ while True:
     chat_history.append(AIMessage(content= result.content))
 
     print("AI: ",result.content)
+
 
 
 
